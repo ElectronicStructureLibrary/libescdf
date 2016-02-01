e3a4d348c4fcbf592cbfacf437e70632adaad5cc
micaeljtoliveira <micaeljtoliveira@gmail.com>
2016-02-01 16:41:46 +0100

    * Added escdf_err_t.

--- src/escdf_error.h
+++ src/escdf_error.h
@@ -47,6 +47,7 @@ struct escdf_error_type {
 };
 typedef struct escdf_error_type escdf_error_t;
 
+typedef int escdf_err_t;
 
 /**********************************************************************
  * Routines                                                           *
