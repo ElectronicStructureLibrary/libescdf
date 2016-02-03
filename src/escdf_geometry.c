37339c7a70bbe261cb4216afb8836c096f194d3e
micaeljtoliveira <micaeljtoliveira@gmail.com>
2016-02-03 09:49:26 +0100

    * Reformating.  * Made escdf_geometry type private.

--- src/escdf_geometry.c
+++ src/escdf_geometry.c
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2016 M. Oliveira
+ Copyright (C) 2016 F. Corseti, M. Oliveira
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
@@ -18,32 +18,61 @@
 */
 
 #include <stddef.h>
+
 #include "escdf_geometry.h"
 
-escdf_geometry_t * escdf_geometry_new(const hid_t file, const char *path)
-{
-    escdf_geometry_t *geometry = (escdf_geometry_t *) malloc(sizeof(escdf_geometry_t));
-    hid_t lapl_id;
-    herr_t herr_status;
-    htri_t htri_status;
 
-    /* check if "/geometry" exists; if not, create it */
-    lapl_id = H5Pcreate(H5P_LINK_ACCESS);
-    htri_status = H5Lexists(file, "/geometry", lapl_id);
-    if (!htri_status) {
-        geometry->group_id = H5Gcreate(file, "/geometry", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-        herr_status = H5Gclose(geometry->group_id);
+/******************************************************************************
+ * Data structures                                                            *
+ ******************************************************************************/
+
+/**
+ * Geometry data type. This type stores all the metadata that can be found in
+ * the ESCDF geometry group. It also stores if any of the other data are
+ * present in the group or not.
+*/
+struct escdf_geometry {
+    hid_t group_id; /**< Handle for HDF5 group */
+
+    /* The metadata */
+    _int_set_t number_of_physical_dimensions;
+    int *dimension_types;
+    _bool_set_t embedded_system;
+
+    /* Information about which data is present in the group */
+    bool magnetic_moment_directions_is_present;
+};
+
+
+/******************************************************************************
+ * Global functions                                                           *
+ ******************************************************************************/
+
+escdf_geometry_t * escdf_geometry_new(const escdf_handle_t *handle,
+        const char *name)
+{
+    escdf_geometry_t *geometry;
+
+    geometry = (escdf_geometry_t *) malloc(sizeof(escdf_geometry_t));
+    FULFILL_OR_RETURN(geometry != NULL, ESCDF_ENOMEM)
+
+    /* check if "geometries" group exists; if not, create it */
+    if (!H5Lexists(handle->group_id, "geometries", H5P_DEFAULT)) {
+        geometry->group_id = H5Gcreate(handle->group_id, "geometries",
+                                       H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    } else {
+        geometry->group_id = H5Gopen(geometry->group_id, "geometries",
+                                     H5P_DEFAULT);
     }
 
     /* check if specific geometry group exists and open it; if not, create it */
-    htri_status = H5Lexists(file, path, lapl_id);
-    if (!htri_status) {
-        geometry->group_id = H5Gcreate(file, path, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    if (!H5Lexists(geometry->group_id, name, H5P_DEFAULT)) {
+        geometry->group_id = H5Gcreate(geometry->group_id, name, H5P_DEFAULT,
+                                       H5P_DEFAULT, H5P_DEFAULT);
     }
     else {
-        geometry->group_id = H5Gopen(file, path, H5P_DEFAULT);
+        geometry->group_id = H5Gopen(geometry->group_id, name, H5P_DEFAULT);
     }
-    herr_status = H5Pclose(lapl_id);
 
     /* no metadata set at the moment */
     geometry->number_of_physical_dimensions.is_set = false;
@@ -64,9 +93,9 @@ void escdf_geometry_free(escdf_geometry_t * geometry)
     herr_status = H5Gclose(geometry->group_id);
 }
 
-escdf_error_t escdf_geometry_read_metadata(escdf_geometry_t *geometry)
+escdf_errno_t escdf_geometry_read_metadata(escdf_geometry_t *geometry)
 {
-    escdf_error_t error;
+    escdf_errno_t error;
     hid_t attribute_id;
     herr_t herr_status;
     htri_t htri_status;
@@ -77,8 +106,10 @@ escdf_error_t escdf_geometry_read_metadata(escdf_geometry_t *geometry)
     htri_status = H5Aexists(geometry->group_id, "number_of_physical_dimensions");
     if (htri_status) {
       geometry->number_of_physical_dimensions.is_set = true;
-      attribute_id = H5Aopen(geometry->group_id, "number_of_physical_dimensions", H5P_DEFAULT);
-      herr_status = H5Aread(attribute_id, H5T_NATIVE_INT, &geometry->number_of_physical_dimensions.value);
+      attribute_id = H5Aopen(geometry->group_id,
+              "number_of_physical_dimensions", H5P_DEFAULT);
+      herr_status = H5Aread(attribute_id, H5T_NATIVE_INT,
+              &geometry->number_of_physical_dimensions.value);
     }
 
     /* read datasets of the group */
@@ -86,9 +117,9 @@ escdf_error_t escdf_geometry_read_metadata(escdf_geometry_t *geometry)
     return error;
 }
 
-escdf_error_t escdf_geometry_write_metadata(const escdf_geometry_t *geometry)
+escdf_errno_t escdf_geometry_write_metadata(const escdf_geometry_t *geometry)
 {
-    escdf_error_t error;
+    escdf_errno_t error;
     hid_t group_id, dataspace_id, attribute_id;
     hsize_t dims;
     herr_t herr_status;
@@ -99,9 +130,11 @@ escdf_error_t escdf_geometry_write_metadata(const escdf_geometry_t *geometry)
     if (geometry->number_of_physical_dimensions.is_set) {
         dims = 1;
         dataspace_id = H5Screate_simple(1, &dims, NULL);
-        attribute_id = H5Acreate2(geometry->group_id, "number_of_physical_dimensions", H5T_NATIVE_INT,
-                                  dataspace_id, H5P_DEFAULT, H5P_DEFAULT);
-        herr_status = H5Awrite(attribute_id, H5T_NATIVE_INT, &geometry->number_of_physical_dimensions.value);
+        attribute_id = H5Acreate2(geometry->group_id,
+                "number_of_physical_dimensions", H5T_NATIVE_INT, dataspace_id,
+                H5P_DEFAULT, H5P_DEFAULT);
+        herr_status = H5Awrite(attribute_id, H5T_NATIVE_INT,
+                &geometry->number_of_physical_dimensions.value);
     }
 
     /* write datasets of the group */
@@ -109,15 +142,14 @@ escdf_error_t escdf_geometry_write_metadata(const escdf_geometry_t *geometry)
     return error;
 }
 
-escdf_error_t escdf_geometry_set_number_of_physical_dimensions(escdf_geometry_t *geometry,
-                                                               const int number_of_physical_dimensions)
+escdf_errno_t escdf_geometry_set_number_of_physical_dimensions(
+        escdf_geometry_t *geometry, const int number_of_physical_dimensions)
 {
-    escdf_error_t error;
+    escdf_errno_t error;
 
     /* checks on the value */
     if (number_of_physical_dimensions != 3) {
-        error.id = 1;
-        return error;
+        return ESCDF_EVALUE;
     }
 
     /* set the value */
@@ -129,19 +161,21 @@ escdf_error_t escdf_geometry_set_number_of_physical_dimensions(escdf_geometry_t 
     return error;
 }
 
-int escdf_geometry_get_number_of_physical_dimensions(const escdf_geometry_t *geometry)
+int escdf_geometry_get_number_of_physical_dimensions(
+        const escdf_geometry_t *geometry)
 {
-    escdf_error_t error;
+    escdf_errno_t error;
 
     /* check if the value is set */
     if (!geometry->number_of_physical_dimensions.is_set) {
-        error.id = 1;
+        error = ESCDF_EVALUE;
     }
 
     return geometry->number_of_physical_dimensions.value;
 }
 
-bool escdf_geometry_is_set_number_of_physical_dimensions(const escdf_geometry_t *geometry)
+bool escdf_geometry_is_set_number_of_physical_dimensions(
+        const escdf_geometry_t *geometry)
 {
     return geometry->number_of_physical_dimensions.is_set;
 }
--- src/escdf_geometry.h
+++ src/escdf_geometry.h
@@ -1,5 +1,6 @@
 /*
- Copyright (C) 2016 D. Caliste, F. Corsetti, M. Oliveira, Y. Pouillon, and D. Strubbe
+ Copyright (C) 2016 D. Caliste, F. Corsetti, M. Oliveira, Y. Pouillon,
+                    and D. Strubbe
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
@@ -26,41 +27,31 @@
 
 #include "utils.h"
 #include "escdf_handle.h"
+#include "escdf_geometry.c"
 
 
-/*****************************************************************************
- * Data structures                                                           *
- *****************************************************************************/
+/******************************************************************************
+ * Data structures                                                            *
+ ******************************************************************************/
 
-/**
- * Geometry data type. This type stores all the metadata that can be found in
- * the ESCDF geometry group. It also stores if any of the other data are
- * present in the group or not.
-*/
-typedef struct  {
-    hid_t group_id; /**< Handle for HDF5 group */
-
-    /* The metadata */
-    _int_set_t number_of_physical_dimensions;
-    int *dimension_types;
-    _bool_set_t embedded_system;
-
-    /* Information about which data is present in the group */
-    bool magnetic_moment_directions_is_present;
-} escdf_geometry_t;
+typedef struct escdf_geometry escdf_geometry_t;
 
 
-/*****************************************************************************
- * Global functions                                                          *
- *****************************************************************************/
+/******************************************************************************
+ * Global functions                                                           *
+ ******************************************************************************/
 
 /**
  * Creates a new instance of the geometry data type.
  *
- * @param[in] path: the path to the ESCDF geometry group.
+ * @param[in] handle: the file/group handle defining the root where to open
+ * the "/geometries" group.
+ * @param[in] name: If NULL, assume that the geometry is stored in the
+ * "/geometries" group, otherwise "/geometries/name" is used.
  * @return instance of the geometry data type.
  */
-escdf_geometry_t * escdf_geometry_new(const hid_t file, const char *path);
+escdf_geometry_t * escdf_geometry_new(const escdf_handle_t *handle,
+        const char *name);
 
 /**
  * Free all memory associated with the geometry group.
@@ -70,9 +61,9 @@ escdf_geometry_t * escdf_geometry_new(const hid_t file, const char *path);
 void escdf_geometry_free(escdf_geometry_t * geometry);
 
 
-/*****************************************************************************
- * Metadata functions                                                        *
- *****************************************************************************/
+/******************************************************************************
+ * Metadata functions                                                         *
+ ******************************************************************************/
 
 /**
  * Given a path to a ESCDF geometry group, this routines opens the group, reads
@@ -83,7 +74,7 @@ void escdf_geometry_free(escdf_geometry_t * geometry);
  * @param[in] path: the path to the ESCDF geometry group.
  * @return error code.
  */
-escdf_error_t escdf_geometry_read_metadata(escdf_geometry_t *geometry);
+escdf_errno_t escdf_geometry_read_metadata(escdf_geometry_t *geometry);
 
 /**
  * Given a geometry data type, it writes all the metadata stored in it to the
@@ -95,7 +86,7 @@ escdf_error_t escdf_geometry_read_metadata(escdf_geometry_t *geometry);
  * @param[in] geometry: instance of the geometry group.
  * @return error code.
  */
-escdf_error_t escdf_geometry_write_metadata(const escdf_geometry_t *geometry);
+escdf_errno_t escdf_geometry_write_metadata(const escdf_geometry_t *geometry);
 
 
 /**
@@ -105,8 +96,8 @@ escdf_error_t escdf_geometry_write_metadata(const escdf_geometry_t *geometry);
  * @param[in] number_of_physical_dimensions: the value of the variable to be set.
  * @return error code.
  */
-escdf_error_t escdf_geometry_set_number_of_physical_dimensions(escdf_geometry_t *geometry,
-                                                     const int number_of_physical_dimensions);
+escdf_errno_t escdf_geometry_set_number_of_physical_dimensions(
+        escdf_geometry_t *geometry, const int number_of_physical_dimensions);
 
 /**
  * Get the value of number_of_physical_dimensions stored in the geometry data
@@ -115,7 +106,8 @@ escdf_error_t escdf_geometry_set_number_of_physical_dimensions(escdf_geometry_t 
  * @param[in] geometry: instance of the geometry group.
  * @return the value of the variable.
  */
-int escdf_geometry_get_number_of_physical_dimensions(const escdf_geometry_t *geometry);
+int escdf_geometry_get_number_of_physical_dimensions(
+        const escdf_geometry_t *geometry);
 
 /**
  * Get a pointer to number_of_physical_dimensions stored in the geometry data
@@ -133,12 +125,13 @@ const int * escdf_geometry_ptr_dimension_types(const escdf_geometry_t *geometry)
  * @param[in] geometry: instance of the geometry group.
  * @return TRUE if the variable has been set, FALSE otherwise.
  */
-bool escdf_geometry_is_set_number_of_physical_dimensions(const escdf_geometry_t *geometry);
+bool escdf_geometry_is_set_number_of_physical_dimensions(
+        const escdf_geometry_t *geometry);
 
 
-/*****************************************************************************
- * Data functions                                                            *
- *****************************************************************************/
+/******************************************************************************
+ * Data functions                                                             *
+ ******************************************************************************/
 
 /**
  * Writes the magnetic_moment_directions variable to the geometry group.
@@ -150,22 +143,28 @@ bool escdf_geometry_is_set_number_of_physical_dimensions(const escdf_geometry_t 
  * @param[in] map:
  * @return error code.
  */
-escdf_error_t escdf_geometry_write_magnetic_moment_directions(const escdf_geometry_t *geometry, const double *buffer,
-                                                            const unsigned int *start, const unsigned int *count,
-                                                            const unsigned int *map);
+escdf_errno_t escdf_geometry_write_magnetic_moment_directions(
+        const escdf_geometry_t *geometry, const double *buffer,
+        const unsigned int *start, const unsigned int *count,
+        const unsigned int *map);
 /**
  * Reads the magnetic_moment_directions variable from the geometry group.
  *
  * @param[in] geometry: instance of the geometry group.
  * @param[out] buffer: the buffer where the data is to be stored in memory.
- * @param[in] start: vector of integers specifying the index in the variable where the first of the data values will be read.
- * @param[in] count: vector of integers specifying the edge lengths along each dimension of the block of data values to be read.
- * @param[in] map: vector of integers that specifies the mapping between the dimensions of the variable and the in-memory structure of the internal data array.
+ * @param[in] start: vector of integers specifying the index in the variable
+ * where the first of the data values will be read.
+ * @param[in] count: vector of integers specifying the edge lengths along each
+ * dimension of the block of data values to be read.
+ * @param[in] map: vector of integers that specifies the mapping between the
+ * dimensions of the variable and the in-memory structure of the internal data
+ * array.
  * @return error code.
  */
-escdf_error_t escdf_geometry_read_magnetic_moment_directions(const escdf_geometry_t *geometry, double *buffer,
-                                                           const unsigned int *start, const unsigned int *count,
-                                                           const unsigned int *map);
+escdf_errno_t escdf_geometry_read_magnetic_moment_directions(
+        const escdf_geometry_t *geometry, double *buffer,
+        const unsigned int *start, const unsigned int *count,
+        const unsigned int *map);
 
 
 
