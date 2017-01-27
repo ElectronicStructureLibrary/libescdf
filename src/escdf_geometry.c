afc97359776e34f55708425fee42d1bf650dfb68
Micael Oliveira <micael.oliveira@mpsd.mpg.de>
2017-01-27 14:47:15 +0100

    Added all missing components from the latest specification to the geometry data structure.

--- src/escdf_geometry.c
+++ src/escdf_geometry.c
@@ -38,16 +38,38 @@ struct escdf_geometry {
     hid_t group_id; /**< Handle for HDF5 group */
 
     /* The metadata */
+    char *system_name;
     _int_set_t number_of_physical_dimensions;
     int *dimension_types;
     _bool_set_t embedded_system;
     _int_set_t number_of_species;
     _int_set_t number_of_sites;
-    _int_set_t absolute_or_reduced_coordinates;
     _int_set_t number_of_symmetry_operations;
+    double *lattice_vector;
+    _int_set_t spacegroup_3D_number;
+    _bool_set_t symmorphic;
+    _bool_set_t time_reversal_symmetry;
+    double *bulk_regions_for_semi_infinite_dimension;
 
     /* Information about which data is present in the group */
-    bool magnetic_moment_directions_is_present;
+    bool species_at_sites_is_present;
+    bool cartesian_site_positions_is_set;
+    bool fractional_site_positions_is_set;
+    bool species_names_is_set;
+    bool chemical_symbols_is_set;
+    bool atomic_numbers_is_set;
+    bool reduced_symmetry_matrices_is_set;
+    bool reduced_symmetry_translations_is_set;
+    bool number_of_species_at_site_is_set;
+    bool concentration_of_species_at_site_is_set;
+    bool local_rotations_is_set;
+    bool magnetic_moments_is_set;
+    bool site_regions_is_set;
+    bool cell_in_host_is_set;
+    bool site_in_host_is_set;
+    bool forces_is_set;
+    bool stress_tensor_is_set;
+
 };
 
 
@@ -61,12 +83,53 @@ escdf_geometry_t * escdf_geometry_new()
 
     geometry = (escdf_geometry_t *) malloc(sizeof(escdf_geometry_t));
 
+    if (geometry == NULL)
+        return NULL;
+
+    /* no metadata set at the moment */
+    geometry->system_name = NULL;
+    geometry->number_of_physical_dimensions.is_set = false;
+    geometry->dimension_types = NULL;
+    geometry->embedded_system.is_set = false;
+    geometry->number_of_species.is_set = false;
+    geometry->number_of_sites.is_set = false;
+    geometry->number_of_symmetry_operations.is_set = false;
+    geometry->lattice_vector = NULL;
+    geometry->spacegroup_3D_number.is_set = false;
+    geometry->symmorphic.is_set = false;
+    geometry->time_reversal_symmetry.is_set = false;
+    geometry->bulk_regions_for_semi_infinite_dimension = NULL;
+
+    /* no data is present at the moment*/
+    geometry->species_at_sites_is_present = false;
+    geometry->cartesian_site_positions_is_set = false;
+    geometry->fractional_site_positions_is_set = false;
+    geometry->species_names_is_set = false;
+    geometry->chemical_symbols_is_set = false;
+    geometry->atomic_numbers_is_set = false;
+    geometry->reduced_symmetry_matrices_is_set = false;
+    geometry->reduced_symmetry_translations_is_set = false;
+    geometry->number_of_species_at_site_is_set = false;
+    geometry->concentration_of_species_at_site_is_set = false;
+    geometry->local_rotations_is_set = false;
+    geometry->magnetic_moments_is_set = false;
+    geometry->site_regions_is_set = false;
+    geometry->cell_in_host_is_set = false;
+    geometry->site_in_host_is_set = false;
+    geometry->forces_is_set = false;
+    geometry->stress_tensor_is_set = false;
+
     return geometry;
 }
 
 void escdf_geometry_free(escdf_geometry_t *geometry)
 {
     if (geometry != NULL) {
+        free(geometry->system_name);
+        free(geometry->dimension_types);
+        free(geometry->lattice_vector);
+        free(geometry->bulk_regions_for_semi_infinite_dimension);
+
         free(geometry);
     }
 }
@@ -74,6 +137,7 @@ void escdf_geometry_free(escdf_geometry_t *geometry)
 escdf_errno_t escdf_geometry_open_group(escdf_geometry_t *geometry, escdf_handle_t *handle, const char *path)
 {
     char group_name[ESCDF_STRLEN_GROUP];
+
 
     if (path == NULL) {
         sprintf(group_name, "%s", "geometries");
@@ -129,33 +193,18 @@ escdf_geometry_t * escdf_geometry_open(const escdf_handle_t *handle,
 
     /* check if "geometries" group exists; if not, create it */
     if (!utils_hdf5_check_present(handle->group_id, "geometries")) {
-        geometry->group_id = H5Gcreate(handle->group_id, "geometries",
-                                       H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        geometry->group_id = H5Gcreate(handle->group_id, "geometries", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
     } else {
-        geometry->group_id = H5Gopen(handle->group_id, "geometries",
-                                     H5P_DEFAULT);
+        geometry->group_id = H5Gopen(handle->group_id, "geometries", H5P_DEFAULT);
     }
 
     /* check if specific geometry group exists and open it; if not, create it */
     if (!utils_hdf5_check_present(geometry->group_id, name)) {
-        geometry->group_id = H5Gcreate(geometry->group_id, name, H5P_DEFAULT,
-                                       H5P_DEFAULT, H5P_DEFAULT);
+        geometry->group_id = H5Gcreate(geometry->group_id, name, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
     }
     else {
         geometry->group_id = H5Gopen(geometry->group_id, name, H5P_DEFAULT);
     }
-
-    /* no metadata set at the moment */
-    geometry->number_of_physical_dimensions.is_set = false;
-    geometry->dimension_types = NULL;
-    geometry->embedded_system.is_set = false;
-    geometry->number_of_species.is_set = false;
-    geometry->number_of_sites.is_set = false;
-    geometry->absolute_or_reduced_coordinates.is_set = false;
-    geometry->number_of_symmetry_operations.is_set = false;
-
-    /* don't yet know which data is present, so set all to false */
-    geometry->magnetic_moment_directions_is_present = false;
 
     return geometry;
 }
@@ -219,13 +268,6 @@ escdf_errno_t escdf_geometry_read_metadata(escdf_geometry_t *geometry)
         return err;
     }
 
-    /* --absolute_or_reduced_coordinates */
-    if ((err = utils_hdf5_read_int(geometry->group_id, "absolute_or_reduced_coordinates",
-                                   &geometry->absolute_or_reduced_coordinates,
-                                   absolute_or_reduced_coordinates_range)) != ESCDF_SUCCESS) {
-        return err;
-    }
-
     /* --number_of_symmetry_operations */
     if ((err = utils_hdf5_read_int(geometry->group_id, "number_of_symmetry_operations",
                                    &geometry->number_of_symmetry_operations,
@@ -252,7 +294,6 @@ escdf_errno_t escdf_geometry_write_metadata(const escdf_geometry_t *geometry)
     FULFILL_OR_RETURN(geometry->embedded_system.is_set, ESCDF_EUNINIT);
     FULFILL_OR_RETURN(geometry->number_of_species.is_set, ESCDF_EUNINIT);
     FULFILL_OR_RETURN(geometry->number_of_sites.is_set, ESCDF_EUNINIT);
-    FULFILL_OR_RETURN(geometry->absolute_or_reduced_coordinates.is_set, ESCDF_EUNINIT);
 
     /* write attributes of the group: */
 
@@ -294,14 +335,6 @@ escdf_errno_t escdf_geometry_write_metadata(const escdf_geometry_t *geometry)
     if ((err = utils_hdf5_write_attr
          (geometry->group_id, "number_of_sites", H5T_STD_U32LE, dims, 1, H5T_NATIVE_INT,
           &geometry->number_of_sites.value)) != ESCDF_SUCCESS) {
-        return err;
-    }
-
-    /* --absolute_or_reduced_coordinates */
-    dims[0] = 1;
-    if ((err = utils_hdf5_write_attr
-         (geometry->group_id, "absolute_or_reduced_coordinates", H5T_STD_U32LE, dims, 1, H5T_NATIVE_INT,
-          &geometry->absolute_or_reduced_coordinates.value)) != ESCDF_SUCCESS) {
         return err;
     }
 
@@ -454,34 +487,6 @@ bool escdf_geometry_is_set_number_of_sites(
         const escdf_geometry_t *geometry)
 {
     return geometry->number_of_sites.is_set;
-}
-
-escdf_errno_t escdf_geometry_set_absolute_or_reduced_coordinates(
-        escdf_geometry_t *geometry, const int absolute_or_reduced_coordinates)
-{
-    /* checks on the value */
-    FULFILL_OR_RETURN(absolute_or_reduced_coordinates == 1 ||
-                      absolute_or_reduced_coordinates == 2, ESCDF_EVALUE);
-
-    /* set the value and status */
-    geometry->absolute_or_reduced_coordinates = _int_set(absolute_or_reduced_coordinates);
-
-    return ESCDF_SUCCESS;
-}
-
-int escdf_geometry_get_absolute_or_reduced_coordinates(
-        const escdf_geometry_t *geometry)
-{
-    /* check if the value is set */
-    FULFILL_OR_RETURN_VAL(geometry->absolute_or_reduced_coordinates.is_set, ESCDF_ESIZE_MISSING, 0);
-
-    return geometry->absolute_or_reduced_coordinates.value;
-}
-
-bool escdf_geometry_is_set_absolute_or_reduced_coordinates(
-        const escdf_geometry_t *geometry)
-{
-    return geometry->absolute_or_reduced_coordinates.is_set;
 }
 
 escdf_errno_t escdf_geometry_set_number_of_symmetry_operations(
